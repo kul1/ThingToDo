de3f365220c89c23e31aa8c5bbcff8fdd75033f4
Prateep Kul <1.0@kul.com>
2014-06-16 12:59:46 +0700

    link unwind buttons

---
+++ XYZAddToDoItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYZAddToDoItemViewController.h
+//  ThingToDo
+//
+//  Created by Kul on 6/16/2557 BE.
+//  Copyright (c) 2557 Kul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAddToDoItemViewController : UIViewController
+
+@end
---
+++ XYZToDoListTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  XYZToDoListTableViewController.h
+//  ThingToDo
+//
+//  Created by Kul on 6/16/2557 BE.
+//  Copyright (c) 2557 Kul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZToDoListTableViewController : UITableViewController
+
+- (IBAction)unwindToList:(UIStoryboardSegue *)segue;
+
+
+@end
